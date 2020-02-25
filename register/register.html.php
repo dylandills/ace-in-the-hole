36240cc84d888cd3ae581d16b059bfb34e656998
Dylan Dills <dylan.dills@gmail.com>
2020-02-25 14:44:23 -0800

    Most stylings done for register page

--- register/register.html.php
+++ register/register.html.php
@@ -32,10 +32,12 @@
         <form method="post" action=" " id="registerForm">
           <!--Athelete or Volunteer-->
           <p>Will you be coming as an Athlete or a Volunteer?</p>
-          <label for="Athlete">Athlete</label>
-          <input type="checkbox" id="myAthlete" name="athlete" value="Athlete">
-          <label for="Volunteer">Volunteer</label>
-          <input type="checkbox" id="myVolunteer" name="volunteer" value="Volunteer">
+          <div class="athlete-wrapper">
+            <label for="Athlete">Athlete</label>
+            <span><input type="checkbox" id="myAthlete" name="athlete" value="Athlete"></span>
+            <label for="Volunteer">Volunteer</label>
+            <span><input type="checkbox" id="myVolunteer" name="volunteer" value="Volunteer"></span>
+          </div>
           <h2>Your Info</h2>
           <label for="myName">Name:</label>
           <input type="text" name="myName" id="myName" required>
@@ -44,12 +46,14 @@
           <label for="myEmail">E-mail:</label>
           <input type="text" name="myEmail" id="myEmail" required>
           <!--Gender Checkboxes-->
-          <label for="female">Female</label>
-          <input type="checkbox" id="myGenderFem" name="female" value="Female">
-          <label for="male">Male</label>
-          <input type="checkbox" id="myGenderMale" name="male" value="Male">
-          <label for="nonbinary">Nonbinary/Other</label>
-          <input type="checkbox" id="myGenderQueer" name="nonbinary" value="Nonbinary">
+          <div class="gender-wrapper">
+            <label for="myGenderFem">Female</label>
+            <span><input type="checkbox" id="myGenderFem" name="myGenderFem"></span>
+            <label for="myGenderMale">Male</label>
+            <span><input type="checkbox" id="myGenderMale" name="myGenderMale"></span>
+            <label for="myGenderQueer">Nonbinary/Other</label>
+            <span><input type="checkbox" id="myGenderQueer" name="myGenderQueer"></span>
+          </div>
           <!--Emergency Contact Info-->
           <h2>Emergency Contact Info</h2>
           <label for="myEName">Emergency Contact Name:</label>
@@ -58,31 +62,33 @@
           <input type="number" id="myENumber" name="myENumber" required>
           <!--Event Registering For-->
           <h2>Event Registering For</h2>
-          <label for="Long Course Swim">Long Course SWIM - 1.2mi</label>
-          <input type="checkbox" id="myLongSwim" name="long course swim" value="Long Course Swim">
-          <label for="Long Course Bike">Long Course BIKE - 58 Miles</label>
-          <input type="checkbox" id="myLongBike" name="long course bike" value="Long Course Bike">
-          <label for="Long Course Run">Long Course RUN - 13.1mi</label>
-          <input type="checkbox" id="myLongRun" name="long course run" value="Long Course Run">
-          <label for="Olympic Swim">OLYMPIC SWIM - 1,500 meters</label>
-          <input type="checkbox" id="myOlySwim" name="olympic swim" value="Olympic Swim">
-          <label for="Olympic Bike">OLYMPIC BIKE - 28mi</label>
-          <input type="checkbox" id="myOlyBike" name="olympic bike" value="Olympic Bike">
-          <label for="Olympic Run">OLYMPIC RUN - 10K</label>
-          <input type="checkbox" id="myOlyRun" name="olympic run" value="Olympic Run">
-          <label for="Sprint">Sprint</label>
-          <input type="checkbox" id="mySprint" name="sprint" value="Sprint">
-          <label for="Try-A-Tri">Try-A-Tri</label>
-          <input type="checkbox" id="myTryATri" name="try-A-tri" value="Try-A-Tri">
-          <label for="Half Marathon Course">Half Marathon COURSE - 13.1-miles</label>
-          <input type="checkbox" id="myHalfMara" name="half marathon course" value="Half Marathon Course">
-          <label for="10k COURSE">10k COURSE</label>
-          <input type="checkbox" id="my10k" name="10k course" value="10k COURSE">
-          <label for="Splash n' Dash">Splash n' Dash</label>
-          <input type="checkbox" id="mySplash" name="splash n' dash" value="Splash n' Dash">
+          <div class="event-wrapper">
+            <label for="myLongSwim">Long Course SWIM - 1.2mi</label>
+            <input type="checkbox" id="myLongSwim" name="myLongSwim">
+            <label for="myLongBike">Long Course BIKE - 58 Miles</label>
+            <input type="checkbox" id="myLongBike" name="myLongBike">
+            <label for="myLongRun">Long Course RUN - 13.1mi</label>
+            <input type="checkbox" id="myLongRun" name="myLongRun">
+            <label for="myOlySwim">OLYMPIC SWIM - 1,500 meters</label>
+            <input type="checkbox" id="myOlySwim" name="myOlySwim">
+            <label for="myOlyBike">OLYMPIC BIKE - 28mi</label>
+            <input type="checkbox" id="myOlyBike" name="myOlyBike">
+            <label for="myOlyRun">OLYMPIC RUN - 10K</label>
+            <input type="checkbox" id="myOlyRun" name="myOlyRun">
+            <label for="mySprint">Sprint</label>
+            <input type="checkbox" id="mySprint" name="mySprint">
+            <label for="myTryATri">Try-A-Tri</label>
+            <input type="checkbox" id="myTryATri" name="myTryATri">
+            <label for="myHalfMara">Half Marathon COURSE - 13.1-miles</label>
+            <input type="checkbox" id="myHalfMara" name="myHalfMara">
+            <label for= "my10k">10k COURSE</label>
+            <input type="checkbox" id="my10k" name="my10k">
+            <label for="mySplash">Splash n' Dash</label>
+            <input type="checkbox" id="mySplash" name="mySplash">
+          </div>
           <!--Special Accomendations-->
-          <label for="myQuestion">Any Special Accommodations?</label>
-          <textarea form="registerForm" rows="10" cols="50" name="myQuestion" id="myQuestion"></textarea>
+          <label for="myAccomendate">Any Special Accommodations?</label>
+          <textarea form="registerForm" rows="10" cols="50" name="myAccomendate" id="myAccomendate"></textarea>
           <!--Submit Button-->
           <input id="mySubmit" type="submit" value="Submit" required>
         </form>
