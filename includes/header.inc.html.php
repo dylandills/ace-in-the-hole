1207d38a0023ad5760bcd091e54eea38fee7cd64
Dylan Dills <dylan.dills@gmail.com>
2020-03-03 16:54:11 -0800

    added img folder and logo.png, as well as inserted logo.png image on headers.inc.html.php

--- includes/header.inc.html.php
+++ includes/header.inc.html.php
@@ -1,6 +1,6 @@
 <?php define('BASE_URL', '/cas222/ace-in-the-hole/'); ?>
 <header class="header">
   <div class="logo">
-    <h3>Logo Image Goes Here</h3>
+    <img src="img/logo.png" alt="Ace In The Hole Logo Image" /> 
   </div>
 </header>
