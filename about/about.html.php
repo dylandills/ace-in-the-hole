f201567365cf002cc89e793cef9a57c6f176551c
Dylan Dills <dylan.dills@gmail.com>
2020-03-07 15:23:27 -0800

    added neccessary content for site, given by client

--- about/about.html.php
+++ about/about.html.php
@@ -15,15 +15,27 @@
     <main>
       <section>
         <h1>About</h1>
-        <p>Etiam venenatis facilisis dolor nec dapibus. Nam condimentum felis augue, nec euismod justo convallis sed. Sed in lorem feugiat, pulvinar purus vel, mollis odio. Sed eget facilisis nisi. Vivamus venenatis ex urna. Integer eget quam vestibulum, lacinia metus vel, ultrices tellus. Quisque fringilla condimentum est, nec posuere massa tincidunt porta. Fusce tempor cursus lacus at congue. Phasellus vel lectus ultricies, hendrerit mauris id, pretium magna. Fusce suscipit commodo malesuada. Suspendisse quis metus sed enim feugiat sagittis. Praesent faucibus felis id vehicula aliquet. Nulla dictum facilisis dapibus. Ut congue, orci vel auctor gravida, velit elit blandit risus, non luctus est quam id orci. Vivamus vel velit a nunc convallis elementum.</p>
-      </section>
-      <section>
-        <h2>Mission Statement</h2>
-        <p>Etiam venenatis facilisis dolor nec dapibus. Nam condimentum felis augue, nec euismod justo convallis sed. Sed in lorem feugiat, pulvinar purus vel, mollis odio. Sed eget facilisis nisi. Vivamus venenatis ex urna. Integer eget quam vestibulum, lacinia metus vel, ultrices tellus. Quisque fringilla condimentum est, nec posuere massa tincidunt porta. Fusce tempor cursus lacus at congue. Phasellus vel lectus ultricies, hendrerit mauris id, pretium magna. Fusce suscipit commodo malesuada. Suspendisse quis metus sed enim feugiat sagittis. Praesent faucibus felis id vehicula aliquet. Nulla dictum facilisis dapibus. Ut congue, orci vel auctor gravida, velit elit blandit risus, non luctus est quam id orci. Vivamus vel velit a nunc convallis elementum.</p>
+        <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds. We offer Events for Every Body.</p>
       </section>
       <section>
         <h2>Annual Ace in the Hole Multisport Weekend</h2>
-        <p>Etiam venenatis facilisis dolor nec dapibus. Nam condimentum felis augue, nec euismod justo convallis sed. Sed in lorem feugiat, pulvinar purus vel, mollis odio. Sed eget facilisis nisi. Vivamus venenatis ex urna. Integer eget quam vestibulum, lacinia metus vel, ultrices tellus. Quisque fringilla condimentum est, nec posuere massa tincidunt porta. Fusce tempor cursus lacus at congue. Phasellus vel lectus ultricies, hendrerit mauris id, pretium magna. Fusce suscipit commodo malesuada. Suspendisse quis metus sed enim feugiat sagittis. Praesent faucibus felis id vehicula aliquet. Nulla dictum facilisis dapibus. Ut congue, orci vel auctor gravida, velit elit blandit risus, non luctus est quam id orci. Vivamus vel velit a nunc convallis elementum.</p>
+        <p>The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.<br />
+        There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!</p>
+      </section>
+      <section>
+        <h2>Course Details</h2>
+        <p>WATER TEMPERATURE is expected to be between 62 & 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.</p><br />
+        <p>Long Course SWIM - 1.2mi - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p><br />
+        <p>Long Course BIKE - 58 Miles: A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.</p><br />
+        <p>Long Course RUN - 13.1mi - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).</p><br />
+        <p>OLYMPIC SWIM - 1,500 meters - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p><br />
+        <p>OLYMPIC BIKE - 28mi - A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.</p><br />
+        <p>OLYMPIC RUN - 10K - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).</p><br />
+        <p>Sprint: Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.</p><br />
+        <p>Try-A-Tri: This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).</p><br />
+        <p>Half Marathon COURSE: Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment. Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified.</p><br />
+        <p>10k COURSE: The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.</p><br />
+        <p>Splash n' Dash: Kids event for participants ages 4 to 15. This is a 100 yard swim and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.</p>
       </section>
       <?php include '../includes/button.inc.html.php'; ?>
     </main>
--- register/register.html.php
+++ register/register.html.php
@@ -15,16 +15,42 @@
     <main>
       <section>
         <h1>Registration</h1>
+        <br />
         <h2>What To Bring</h2>
-        <p>Etiam venenatis facilisis dolor nec dapibus. Nam condimentum felis augue, nec euismod justo convallis sed. Sed in lorem feugiat, pulvinar purus vel, mollis odio. Sed eget facilisis nisi. Vivamus venenatis ex urna. Integer eget quam vestibulum, lacinia metus vel, ultrices tellus.</p>
+        <strong>Watch the weather closely. The show goes on no matter what the weather is doing.</strong><br /><br />
+        <p>You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip timing piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, you will have a very difficult time getting off your wet suit.<br /><br />
+        Swim: Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want to bring your own goggles.<br /><br />
+        Bike: A biking helmet is mandatory. You will also receive 2 stickers in your package with your race number on them. The small sticker should go on the front of your helmet. The bigger sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For safety reasons, be sure to have end-caps on the end of your handle-bars.<br /><br />
+        Run: You must finish the race with your bib number on the front of you. Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don't have to worry about it. Others pin it on a singlet that they put on once they finish the swim before they head out for the bike. Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave for the run. Use whatever option feels best for you.<br /><br />
+        Remember to bring a change of clothing so you can enjoy the post-event festivities.</p>
       </section>
       <section>
         <h2>Prices</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin interdum lorem ante, vel dignissim massa eleifend vitae. In sagittis placerat sem, consequat consectetur turpis consequat sit amet. Sed sollicitudin posuere augue in suscipit.</p>
+        <ul>
+          <li>Long Course - $240</li>
+          <li>Olympic - $110</li>
+          <li>10K - $50</li>
+          <li>Half Marathon - $75</li>
+          <li>Sprint - $90</li>
+          <li>Try-a-Tri - $65</li>
+        </ul>
+        <h2>Cost Includes</h2>
+        <ul>
+          <li>Food & Beer</li>
+          <li>Access to the weekend's live entertainment & Fitness Expo</li>
+          <li>Commemorative Finisher medal</li>
+          <li>Accurate Chip Timing for competitive races</li>
+          <li>ce in the Hole Multisport Weekend Tech Shirt</li>
+          <li>Post-event party & entertainment</li>
+        </ul>
+        <br />
+        <strong>NOTE: Tech shirts guaranteed to pre-registered participants only.</strong>
       </section>
       <section>
         <h2>Packet Pickup</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget quam mattis, pharetra purus vitae, facilisis ante. Maecenas nulla ex, lacinia at erat a, semper finibus eros. </p>
+        <p>All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR. Packet pick up hours: Thursday 8-5, Friday 9-noon. No day of event packet pick up.</p>
+        <br />
+        <strong>For More Information On Course Details, please check out our <a href="<?php echo BASE_URL; ?>about">about</a> page</strong>
       </section>
       <section>
         <h2>Register</h2>
